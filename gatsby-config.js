603911dbb8a0eb48900970379f645c2ab89fbbdb
Maciej Sykuła <maciek.sykula@gmail.com>
2019-06-04 08:12:35 +0200

    Update SEO data and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Your project starts here`,
+    description: `Software development focused on MVP, full web applications and digital product design.`,
+    author: `@ViewSource`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `View Source`,
+        short_name: `ViewSource`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
